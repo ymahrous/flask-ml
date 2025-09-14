b7f9c7f16c4b83cdfea921ee2f42a8bb0ed3ebbf
Yousef Mahrous <84485003+ymahrous@users.noreply.github.com>
2025-09-14 19:35:29 +0300

    init

--- test_mlib.py
+++ test_mlib.py
@@ -1,9 +1,8 @@
-from mlib import format_input, scale_input, height_human
-import numpy as np
 import pytest
-from click.testing import CliRunner
+import numpy as np
 from app import app as flask_app
-
+# from click.testing import CliRunner
+from mlib import format_input, scale_input, height_human
 
 @pytest.fixture
 def test_array():
@@ -11,33 +10,26 @@ def test_array():
     feature = val.reshape(-1, 1)
     return feature
 
-
 @pytest.fixture
 def app():
     yield flask_app
-
 
 @pytest.fixture
 def client(app):
     return app.test_client()
 
-
 def test_format_input(test_array):
     assert test_array.shape == format_input(2).shape
 
-
 def test_scale_input(test_array):
     assert int(scale_input(test_array)) == int(np.array([[-9.56513601]]))
-
 
 def test_height_human():
     assert "6 foot, 1 inches" == height_human(73.4)
     assert "5 foot, 11 inches" == height_human(71.1)
 
-
-# Smoke test Flask
 def test_index(app, client):
     res = client.get("/")
     assert res.status_code == 200
-    expected = "{"message": "Predict the height From weight of MLB players"}"
+    expected = '{"message": "Predict the height From weight of MLB players"}\n'
     assert expected in res.get_data(as_text=True)
